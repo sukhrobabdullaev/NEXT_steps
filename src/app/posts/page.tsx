798e5e937deca5f6823d0a8e5895a9bc15244692
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-02 14:53:56 +0500

    font optimization

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
-
+import { roboto_mono } from "../fonts/font";
 const Posts = () => {
   return (
     <div className="relative w-80 h-80">
+      <h1 className={roboto_mono.className}>Posts </h1>
       <Image
         src="next.svg"
         alt="vercel logo"
-        // width={300}
-        // height={300}
+        width={500}
+        height={500}
         // style={{
         //   maxWidth: "100%",
         //   height: "auto",
         // }}
-        fill
         placeholder="blur"
         blurDataURL="data:..."
         quality={100}
+        layout="responsive"
       />
     </div>
   );
